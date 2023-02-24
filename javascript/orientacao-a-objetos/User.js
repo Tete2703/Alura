71e81b22fb8d0a3799390363d01f31aadcaec954
terezinha <tfsneta@hotmail.com>
2023-02-23 23:03:30 -0300

    Create User.js
    alura

---
+++ javascript/orientacao-a-objetos/User.js
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(nome, email, nascimento, role, ativo = true) {
+        this.nome = nome;
+        this.email = email;
+        this.nascimento = nascimento;
+        this.role = role || 'Estudante';
+        this.ativo = ativo
+    }
+}
